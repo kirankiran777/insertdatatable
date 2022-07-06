510b09ea288956abf077feed06c8a983fa9a915c
kirankiran7777 <kirankannemadugu777@gmail.com>
2022-07-06 19:25:30 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
+from pydoc_data.topics import topics
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+from app.models import *
+
+def display_topic(request):
+    LOT=Topic.objects.all()
+    LOW=Webpage.objects.all()
+    d={'topics':LOT,'webpages':LOW}
+    return render(request,'display_topic.html',d)
